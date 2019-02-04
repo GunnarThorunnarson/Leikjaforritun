95984d6fa851f09176c4dcb916a15029e340f3db
Hrólfur Gylfason <hrolfurgy329@nemi.tskoli.is>
2019-02-04 12:29:26 +0000

    tími

---
+++ FORR2GL05DU/Rúllandi bolti/Assets/Skriftur/MyndavélaStjórnandi.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyndavélaStjórnandi : MonoBehaviour {
+
+    public GameObject spilari;
+
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start() {
+        offset = transform.position - spilari.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate() {
+        transform.position = spilari.transform.position + offset;
+    }
+}
---
+++ FORR2GL05DU/Rúllandi bolti/Assets/Skriftur/SpilaraStjórnandi.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpilaraStjórnandi : MonoBehaviour {
+    public float hraði;
+    private Rigidbody rb;
+
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate () {
+        float hreifingLárétt = Input.GetAxis ("Horizontal");
+        float hreifingLóðrétt = Input.GetAxis ("Vertical");
+
+        Vector3 hreifing = new Vector3 (hreifingLárétt, 0, hreifingLóðrétt);
+
+        rb.AddForce (hreifing * hraði);
+    }
+}
