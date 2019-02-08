9152a9fb7c2557691e2fe68609c0f2d4f72c6ddb
hrolfurgylfa <hrolfurgylfa@protonmail.com>
2019-02-08 12:41:15 +0000

    Rúllandi Bollti tilbúið

--- FORR2GL05DU/Rúllandi bolti/Assets/Skriftur/SpilaraStjórnandi.cs
+++ FORR2GL05DU/Rúllandi bolti/Assets/Skriftur/SpilaraStjórnandi.cs
@@ -1,13 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpilaraStjórnandi : MonoBehaviour {
+
     public float hraði;
+    public Text stigaTextabox;
+    public string stigaTexti;
+    public Text sigurTextabox;
+    public string sigurTexti;
+    public float fjoldiMarkmida;
+
     private Rigidbody rb;
+    private float stig;
 
     void Start () {
         rb = GetComponent<Rigidbody>();
+        stig = 0;
+        BreytaStigaTexta();
+        sigurTextabox.text = "";
     }
 
     void FixedUpdate () {
@@ -17,5 +29,18 @@ public class SpilaraStjórnandi : MonoBehaviour {
         Vector3 hreifing = new Vector3 (hreifingLárétt, 0, hreifingLóðrétt);
 
         rb.AddForce (hreifing * hraði);
+    }
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Markmið")) {
+            other.gameObject.SetActive(false);
+            stig += 1;
+            BreytaStigaTexta();
+            if (stig >= fjoldiMarkmida) {
+                sigurTextabox.text = sigurTexti;
+            }
+        }
+    }
+    void BreytaStigaTexta() {
+        stigaTextabox.text = stigaTexti + stig.ToString();
     }
 }
